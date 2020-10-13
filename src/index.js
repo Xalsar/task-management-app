12498a2ebe079be7730a57ec34d58030f6773c0c
Xalsar <xavicarreragimbert@gmail.com>
2020-10-13 21:13:53 +0200

    Add or upgrade new User routes

--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ const port = 3000
 
 app.use(express.json())
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find()
-        res.status(200).send(users)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
 app.post('/new-user', async (req, res) => {
     const user = new User(req.body)
 
@@ -27,6 +18,15 @@ app.post('/new-user', async (req, res) => {
     }
 })
 
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).send(users)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 app.get('/user/:id', async (req, res) => {
     const _id = req.params.id
 
@@ -34,6 +34,27 @@ app.get('/user/:id', async (req, res) => {
         const user = await User.findById(_id)
         if (!user) {
             return res.status(400).send("No user with this Id!")
+        }
+
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.patch('/update-user/:id', async (req, res) => {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: req.body },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+
+        if (!user) {
+            res.status(400).send()
         }
 
         res.status(200).send(user)
