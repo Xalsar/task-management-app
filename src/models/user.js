918a2f8d8ea33435eb3042d5f4bbb67fc8ac6c93
Xalsar <xavicarreragimbert@gmail.com>
2020-10-14 09:59:10 +0200

    Add tokens and its functions to User model

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -41,6 +42,12 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
 userSchema.pre('save', async function (next) {
@@ -67,6 +74,17 @@ userSchema.statics.findByCredentials = async (email, password) => {
     }
 
     return user
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, '-u+G_P]R,J3.ha7Hecq}.,yqn{.}~AFJQXu{3Hv})nCd[5a6hMN-AQk')
+
+    user.tokens = user.tokens.concat({ token })
+
+    await user.save()
+
+    return token
 }
 
 const User = mongoose.model('User', userSchema)
