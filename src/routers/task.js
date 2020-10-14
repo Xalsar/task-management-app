1b5eec6e7ebf9975aae1b5ec2b4ccfa887af1c20
Xalsar <xavicarreragimbert@gmail.com>
2020-10-14 12:07:39 +0200

    Add auth to task routes

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 
-router.post('/task/create', async (req, res) => {
-    const allowedFields = ["title", "description", "finishDate"]
+router.post('/task/create', auth, async (req, res) => {
+    const allowedFields = ["title", "description", "finishDate", "owner"]
     const isValidUpdate = Object.keys(req.body).every((field) => {
         return allowedFields.includes(field)
     })
@@ -13,7 +14,10 @@ router.post('/task/create', async (req, res) => {
         return res.status(500).send("You are trying to update an invalid field!")
     }
 
-    const task = new Task(req.body)
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
 
     try {
         await task.save()
@@ -23,43 +27,57 @@ router.post('/task/create', async (req, res) => {
     }
 })
 
-router.get('/task/list', async (req, res) => {
+router.get('/task/list', auth, async (req, res) => {
     try {
-        const tasks = await Task.find()
+        const tasks = await Task.find({ owner: req.user._id })
         res.status(200).send(tasks)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
-router.get('/task/update/:id', async (req, res) => {
+router.patch('/task/update/:id', auth, async (req, res) => {
     try {
         const _id = req.params.id
+        
+        const updates = Object.keys(req.body)
+        const allowedFields = ["title", "description", "finishDate"]
+        const isValidUpdate = updates.every((field) => {
+            return allowedFields.includes(field)
+        })
 
-        const task = await Task.findOneAndUpdate(
-            { _id },
-            { $set: req.body },
-            {
-                new: true,
-                runValidators: true
-            }
-        )
+        if (!isValidUpdate) {
+            return res.status(500).send("You are trying to update an invalid field!")
+        }
+
+        const task = await Task.findOne({
+            _id,
+            owner: req.user._id
+        })
 
         if (!task) {
             return res.status(500).send()
         }
 
-        res.status(500).send(task)
+        updates.forEach((update) => task[update] = req.body[update])
+
+        await task.save()
+
+        res.status(400).send(task)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
-router.get('/task/find/:id', async (req, res) => {
+router.get('/task/find/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.find({
+            _id,
+            owner: req.user._id
+        })
+
         if (!task) {
             return res.status(500).send()
         }
@@ -70,13 +88,18 @@ router.get('/task/find/:id', async (req, res) => {
     }
 })
 
-router.delete('/task/delete/:id', async (req, res) => {
+router.delete('/task/delete/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.find({
+            _id: req.params._id,
+            owner: req.user._id
+        })
 
         if (!task) {
             return res.status(500).send(task)
         }
+
+        await task.delete()
 
         res.status(200).send(task)
     } catch (error) {
