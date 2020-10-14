de10e3dd0f22d0392045f0953df6fcbe79f0746f
Xalsar <xavicarreragimbert@gmail.com>
2020-10-14 12:07:24 +0200

    Return token when create user

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,9 +8,9 @@ router.post('/user/create', async (req, res) => {
     const user = new User(req.body)
 
     try {
-        await user.generateAuthToken()
+        const token = await user.generateAuthToken()
         await user.save()
-        res.status(201).send(user)
+        res.status(201).send({ user, token })
     } catch (error) {
         res.status(500).send(error)
     }
