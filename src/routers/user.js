0c0964ed124ea16e699963308b79e085fac586fb
Xalsar <xavicarreragimbert@gmail.com>
2020-10-14 10:16:00 +0200

    Add authentication and login and log out routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 
@@ -7,6 +8,7 @@ router.post('/user/create', async (req, res) => {
     const user = new User(req.body)
 
     try {
+        await user.generateAuthToken()
         await user.save()
         res.status(201).send(user)
     } catch (error) {
@@ -21,6 +23,16 @@ router.get('/user/list', async (req, res) => {
     } catch (error) {
         res.status(500).send(error)
     }
+})
+
+router.get('/user/me', auth, async (req, res) => {
+    const user = req.user
+
+    if (!user) {
+        res.status(500).send(user)
+    }
+
+    res.status(200).send(user)
 })
 
 router.get('/user/find/:id', async (req, res) => {
@@ -85,9 +97,24 @@ router.delete('/user/delete/:id', async (req, res) => {
 router.post('/user/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
+        await user.generateAuthToken()
         res.status(200).send(user)
     } catch (error) {
         res.status(400).send(error)
+    }
+})
+
+router.post('/user/logout', auth, async (req, res) => {
+    try {
+        const user = req.user
+        const tokens = user.tokens.filter((token) => token.token !== req.header('Auth'))
+
+        user.tokens = tokens
+        await user.save()
+
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send()
     }
 })
 
